4c73dd67f83b27da86d38af0973b2adbc020cd1c
Liam McDonald <101204247+macglencoe@users.noreply.github.com>
2022-03-23 02:26:06 +0000

    fix 3/22/2022

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def overviewpage():
                     #break
         with st.spinner('Logging in...'):
             st.info(
-                'Loading all of your courses can be pretty time-consuming. Luckily, if you have cookies enabled, you won\'t have to wait every time.'
+                'Loading all of your courses can be pretty time-consuming.\n Luckily, if you have cookies enabled, you won\'t have to wait every time.'
             )
             scdata.threelegged(st.session_state)
             #scdata.twolegged(st.session_state)
