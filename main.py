010328150cd271f62706b0d2a08f43bea3088879
Liam McDonald <101204247+macglencoe@users.noreply.github.com>
2022-03-23 12:53:06 +0000

    fix 3/23/2022

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def overviewpage():
     else:
         scdata.save_userstate(st.session_state)
     st.write(scdata.user_states)
-    
+    st.write(st.session_state)
     if 'logged_in' not in st.session_state:
         with st.spinner('Loading courses...'):
             st.info(
