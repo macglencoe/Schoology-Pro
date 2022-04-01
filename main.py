d97a1410cbd4a735566b8a973aca7d01c9416811
Liam McDonald <101204247+macglencoe@users.noreply.github.com>
2022-04-01 15:46:01 +0000

    fix 4/1/2022

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ def display_perchart(sec,per):
             r'\ \end{array} \begin{array}{r}'+
             addstring+r'\hline\ ='+totalfactor+r'\:%\end{array}'
         )
-        st.markdown(r'$$\frac{1}{2}$$')
+        st.markdown(r'$$\begin{array}{r}85\times0.37=\\15\times1.0=\\\ \end{array}\begin{array}{r}32\\15.0\\\hline\ =47\:%\end{array}$$')
     if dfid in st.session_state.percharts:
         st.altair_chart(
             st.session_state.percharts[dfid],
