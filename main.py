b36ef06fc9ad40d32c518fd967fa0e96185a9f58
Liam McDonald <101204247+macglencoe@users.noreply.github.com>
2022-04-05 18:13:24 +0000

    fix 4/5/2022

--- main.py
+++ main.py
@@ -550,7 +550,7 @@ with st.sidebar:
             st.experimental_rerun()
         else:
             st.error('Your data is either already cleared or not saved yet!')
-    debug = st.checkbox('debug',key='debugbutton')
+    debug = st.checkbox('debug',key='debugcheck')
     if debug and st.session_state.me['name_display'] == 'LIAM MCDONALD':
         st.session_state['debug'] = True
         if st.checkbox('Show Users'):
