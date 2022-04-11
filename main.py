166bca31c549e7b1f451687a459326fcc75ff8fd
macglencoe <101204247+macglencoe@users.noreply.github.com>
2022-04-11 11:25:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ def homepage():
         st.experimental_set_query_params(
             page='GPA'
         )
-    del clicked
 def login():
     if 'logged_in' not in st.session_state:
         #st.session_state['auth'] = scdata.get_auth()
