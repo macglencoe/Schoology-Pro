0f9229c2d983d2bbf5b06cd128442ff32bc9ab61
macglencoe <101204247+macglencoe@users.noreply.github.com>
2022-04-27 10:05:33 -0400

    Demo loading function and others

--- schoologydata.py
+++ schoologydata.py
@@ -121,6 +121,8 @@ class DemoCategory:
             weight,method,ss
         ):
         self.id = id
+        if id in session_state['_democategories']:
+            return
         self.title = title
         self.course_id = course_id
         self.weight = weight
@@ -201,7 +203,88 @@ def loadcourse(session_state):
         for category in sc.get_grading_categories(course['section_id']):
             Category(category,session_state)
     return returncourses 
+
+def demoload(
+        session_state,catcount=3,asgncount=5
+    ):
+    _courses = session_state['_democourses']
+    _periods = session_state['_demoperiods']
+    sel_string = session_state['selected_democourse']
+    loaded_courses = session_state['loaded_democourses']
+    matches = []
+    returncourses = []
+    course_titles = [
+        "PERSONAL FINANCE", "COMP SCI", "GRAPHIC DESIGN",
+        "ENGLISH XII", "CREATIVE WRITING", "HOME ECONOMICS",
+        "SPANISH II", "FRENCH II", "ALGEBRA II", "CALCULUS",
+        "GEOMETRY", "BAND", "ORCHESTRA", "DRAMA",
+        "PHYSICAL EDUCATION", "BIOLOGY", "PHYSICS",
+        "PSYCHOLOGY", "CIVICS/GOVT", "WORLD HISTORY",
+        "PHOTOGRAPHY", "WOODWORKING", "AGRICULTURE"
+    ]
+    courses = [(title,random.randrange(1,100)) for title in course_titles]
+
+    periods = [
+        ("1st Quarter",1),("2nd Quarter",2),
+        ("3rd Quarter",3),("4th Quarter",4)
+    ]
     
+    categories = [
+        (f"Category {x}",random.randrange(1,10000)) for x in range(catcount)
+    ]
+
+    assignments = [
+        (f"Assignment {x}",random.randrange(1,10000)) for x in range(asgncount)
+    ]
+    if sel_string in loaded_courses:
+        return demoreloadcourse(sel_string, session_state)
+
+    for c in courses:
+        coursetitle = c[0]
+        if sel_string in coursetitle or coursetitle in sel_string:
+            matches.append(c)
+    if len(matches) == 0:
+        print(f"Course \"{sel_string}\" couldn't be found")
+        return
+    for sec in matches:
+        categories = [
+            (f"Category {x}",random.randrange(1,10000)) for x in range(catcount)
+        ]
+        totalweight = 100
+
+        if sec[1] not in _courses.keys():
+            DemoCourse(
+                sec[0],sec[1],
+                session_state
+            )
+        returncourses.append(
+            _courses[sec[1]]
+        )
+        for per in periods:
+            if per[1] not in _periods.keys():
+                DemoPeriod(per[1],per[0], session_state)   
+            for cat in categories:
+                isweighted = random.choice([True,False])
+                if isweighted:
+                    weight = random.randint(1,totalweight)
+                     totalweight -= weight
+                else:
+                    weight = 0
+                DemoCategory(
+                    cat[1],cat[0],sec[1],
+                    weight, random.choice([1,2]),
+                    session_state
+                )
+                for asg in assignments:
+                    max = random.randrange(1,101)
+                    grade = random.randint(0,max)
+                    DemoAssignment(
+                        asg[1],asg[0],grade,max,
+                        sec[1], per[1], cat[1]
+                        session_state
+                    )
+    return returncourses
+
 def reloadcourse(sel_string, session_state):
     olist = session_state['olist']
     _courses = session_state['_courses']
@@ -211,6 +294,13 @@ def reloadcourse(sel_string, session_state):
             returncourses.append(c)
     return returncourses
     
+def demoreloadcourse(sel_string, session_state):
+    _courses = session_state['_democourses']
+    returncourses = []
+    for c in _courses.values():
+        if c.title == sel_string:
+            returncourses.append(c)
+    return returncourses
 
 apikey = '65d4d1f05710a0eb66658122d7cc426e062100b60'
 apisecret = 'f90c6f8faa564767831e6c4c41acb4f4'
