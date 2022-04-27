253d3c4c970db21a0bbc67ce4d7a4fe0950c9d24
macglencoe <101204247+macglencoe@users.noreply.github.com>
2022-04-27 11:58:10 -0400

    Update schoologydata.py

--- schoologydata.py
+++ schoologydata.py
@@ -251,11 +251,11 @@ def demoload(
             _courses[sec[1]]
         )
         for x,per in enumerate(periods):
-            continue
             if str(x+1) not in _periods.keys():
                 DemoPeriod(str(x+1),per,session_state)
             totalweight = 100   
             for cat in categories:
+                continue
                 isweighted = random.choice([True,False])
                 if isweighted:
                     weight = random.randint(1,totalweight)
