36c35ffe7578738a03f4cb82c1f3f01d31d3fa5c
macglencoe <101204247+macglencoe@users.noreply.github.com>
2022-04-27 12:28:00 -0400

    Update demomodule.py

--- demomodule.py
+++ demomodule.py
@@ -1,6 +1,6 @@
 import schoologydata as scdata
 
-def demo_overviewpage(st,params):
+def overviewpage(st,params):
     go_home = st.button('Return to Home')
     
     if "demo_generated" not in st.session_state:
@@ -35,12 +35,9 @@ def demo_overviewpage(st,params):
             for m in matches:
                 period_grades = []
                 st.title(f'{m.title}')
-                st.write(m.periods)
-                st.write(st.session_state['_demoperiods'])
                 for id in m.periods:
                     if id in st.session_state['_demoperiods']:
-                        period = st.session_state['_demoperiods'][id]
-                        st.write(period.id)
+                        period = st.session_state['_demoperiods'][id])
                         dfid = f'{m.id} {period.id}'
                         if st.button(
                             period.title,
@@ -81,3 +78,157 @@ def demo_overviewpage(st,params):
         st.experimental_set_query_params(
             page='Home'
         )
+
+def display_categories(st,params,m,p):
+    for cat in st.session_state['_democategories'].values():
+        if cat.course_id == m.id:
+            st.subheader(cat.title)
+            with st.expander('Edit Assignments'):
+                has_asgs = asg_editors(st,cat,p,m)
+            if has_asgs:
+                display_catchart(st,m,p,cat)
+
+def display_catchart(st,m,p,c):
+    dataframe_id = (f'{m.id} {p.id} {c.id}')
+    period_dfid = f'{m.id} {p.id}'
+    rerun = False if period_dfid in st.session_state.demopercharts else True
+    method = st.radio(
+        'Calculation Type',
+        ('Point Average','Percent Average'),
+        help='Compare the different methods with your grade on Schoology to determine which method that category uses.',
+        key= f'demomethod {dataframe_id}',
+        on_change = del_chart,args=([st,dataframe_id])
+    )
+    advanced = st.checkbox(
+        'Advanced',
+        key = f'demoadvanced {dataframe_id}'
+    )
+
+    if True:
+        if method == 'Point Average' and advanced:
+            df = st.session_state.demodataframes[dataframe_id]
+            display_df = df[['title','grade','max','url']].copy()
+            total_earned = str(df['grade'].sum())
+            total_max = str(df['max'].sum())
+            gradefloat = round(df['grade'].sum()/df['max'].sum(), 4)
+            gradedecimal = str(gradefloat)
+            gradepercent = str(gradefloat*100)
+            st.dataframe(display_df)
+            st.latex(
+                r'\frac{'+total_earned+r'}{'+total_max+r'}='+gradedecimal+r'\times100='+gradepercent+r'%'
+            )
+    if dataframe_id in st.session_state.democharts:
+        st.altair_chart(
+            st.session_state.democharts[dataframe_id],
+            use_container_width=True
+        )
+        return
+    if method == 'Point Average':
+        c.method = 2
+        chart = pointaverage_chart(c,p,m)
+        if chart:
+            st.altair_chart(
+                chart,
+                use_container_width=True
+            )
+            st.experimental_rerun()
+                
+    if method == 'Percent Average':
+        c.method = 1
+        chart = percentaverage_chart(c,p,m)
+        if chart:
+            st.altair_chart(
+                chart,
+                use_container_width=True
+            )
+            st.experimental_rerun()
+        else:
+            print('chart fail')
+    if rerun:
+        st.experimental_rerun()
+
+def asg_editors(st,cat,per,sec):
+    dfid = f'{sec.id} {per.id} {cat.id}'
+    asglist = [asg for asg in
+              st.session_state['_demoassignments'].values() if
+              asg.category == cat.id and
+              asg.period == per.id and
+              asg.section_id == sec.id]
+    if len(asglist) == 0:
+        st.error(
+            'No assignments were found in this category!\nIf you think this is incorrect, please [tell me about it](https://github.com/macglencoe/Schoology-Pro#bugs-and-feature-requests)'
+        )
+        if st.checkbox(
+            'Manually set score',
+            key = f'demomanualscore {dfid}'
+        ):
+            gradeselect = st.number_input(
+                'Grade out of 100',
+                key = f'demogradeselect {dfid}',
+                value = 0,
+            )
+            df = pd.DataFrame([
+            {
+                    'title' : "Should Not Be Seen",
+                    'max' : 100,
+                    'grade' : gradeselect
+                }
+            ])
+            st.session_state.demodataframes[dfid] = df
+        else:
+            if dfid in st.session_state.demodataframes:
+                del st.session_state.demodataframes[dfid]
+        return False
+    select_asg = st.selectbox(
+        'Select an assignment',
+        [asg.title for asg in asglist],
+        key = f'selected_demoassignment {dfid}',
+        on_change = del_chart,args=([st,dfid])
+    )
+    for asg in asglist:
+        if asg.title == select_asg:
+            break
+    excused = st.checkbox(
+        'Excused',
+        value = True if asg.grade is None else False,
+        key = f'demoexcused {asg.id}',
+        on_change = del_chart,args=([st,dfid])
+    )
+    notassigned = st.checkbox(
+        'Not Assigned',
+        value = True if asg.max is None else False,
+        key = f'demonotassigned {asg.id}'
+    )
+    if notassigned:
+        asg.max = None
+    if excused:
+        asg.grade = None
+    if not excused and asg.grade is None:
+        asg.grade = 0
+    if not notassigned and asg.max is None:
+        asg.max = 1
+    if not notassigned and not excused:
+        asg.grade = st.number_input(
+            'Earned Points',
+            min_value = 0,
+            value = int(asg.grade),
+            key = f'demoearned {asg.id}',
+            on_change = del_chart,args=([st,dfid])
+        )
+        asg.max = st.number_input(
+            'Maximum Points',
+            min_value = 0,
+            value = int(asg.max),
+            key = f'demomax {asg.id}',
+            on_change = del_chart,args=([st,dfid])
+        )
+        asg.percent = round(asg.grade/asg.max,3)
+    return True
+
+
+def del_chart(st,dataframe_id):
+    sec_id,per_id,cat_id = dataframe_id.split()
+    if dataframe_id in st.session_state.charts:
+        del st.session_state.charts[dataframe_id]
+    else:
+        st.error('No category chart found')
