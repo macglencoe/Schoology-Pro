edb1cf4838ec333d37a23225dbb7f4ea0bb84a78
macglencoe <101204247+macglencoe@users.noreply.github.com>
2022-04-27 11:40:59 -0400

    Update schoologydata.py

--- schoologydata.py
+++ schoologydata.py
@@ -252,7 +252,7 @@ def demoload(
         )
         for x,per in enumerate(periods):
             DemoPeriod(str(x+1),per,session_state)
-            _periods[per] = len(periods)
+            _periods[per] = periods
             totalweight = 100   
             for cat in categories:
                 isweighted = random.choice([True,False])
