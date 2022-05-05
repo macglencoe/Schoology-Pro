3be647ed8e1666b4705638f0b8671e25aaedad23
Liam McDonald <101204247+macglencoe@users.noreply.github.com>
2022-05-05 22:17:20 +0000

    fix

--- main.py
+++ main.py
@@ -74,8 +74,7 @@ def settings():
             page='Course'
         )
     if 'logged_in' in st.session_state:
-        st.write('You are logged in as %s' % st.session_state['me']['name_display'].lower().title())
-        st.header('Hello, %s!' % st.session_state['me']['name_display'])
+        st.header('Hello, %s!' % st.session_state['me']['name_display'].lower().title())
         if st.button('Log Out'):
             get_auth_cached(reset=True)
             st.session_state.clear()
